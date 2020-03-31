d4b7f959847879fce7af6a4c56c9cbabd4809d17
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-03-31 20:29:05 +0200

    Setting listener on clicking submit button

--- app/src/main/java/com/sallefy/activity/LoginActivity.java
+++ app/src/main/java/com/sallefy/activity/LoginActivity.java
@@ -2,31 +2,51 @@ package com.sallefy.activity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sallefy.R;
 import com.sallefy.managers.authentication.AuthenticationCallback;
+import com.sallefy.managers.authentication.AuthenticationManager;
 import com.sallefy.model.JWTToken;
 
 public class LoginActivity extends AppCompatActivity implements AuthenticationCallback {
 
     private Button mLoginButton;
+    private EditText mUsernameField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // mLoginButton = findViewById(R.id)
+        initView();
+    }
+
+    private void initView() {
+        mUsernameField = findViewById(R.id.login_form_field_username);
+        mPasswordField = findViewById(R.id.login_form_field_password);
+        mLoginButton = findViewById(R.id.login_form_submit_button);
+        mLoginButton.setOnClickListener(listener -> {
+            String username = mUsernameField.getText().toString();
+            String password = mPasswordField.getText().toString();
+            attemptLogin(username, password);
+        });
+    }
+
+    private void attemptLogin(String username, String password) {
+        AuthenticationManager.getInstance().authenticate(username, password, this);
     }
 
     @Override
     public void onAuthenticationSuccess(JWTToken token) {
-
+        Toast.makeText(this, token.getToken(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAuthenticationFailure(Throwable throwable) {
-
+        Toast.makeText(this, "Error authenticating", Toast.LENGTH_SHORT).show();
     }
 }
