689628b8ee54a75deabac45c48a5c13153eb209b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-13 18:21:27 +0200

    Removed token store manager

--- app/src/main/java/com/sallefy/services/authentication/AuthenticationUtils.java
+++ app/src/main/java/com/sallefy/services/authentication/AuthenticationUtils.java
@@ -3,15 +3,18 @@ package com.sallefy.services.authentication;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.sallefy.model.JWTToken;
+import com.sallefy.model.User;
+
 import static java.util.Objects.isNull;
 
 public class AuthenticationUtils {
 
     private static String LOGIN_COLLECTION = "userPreferences";
     private static String KEY_USER = "username";
-    private static String KEY_PASSWORD = "password";
+    private static String KEY_TOKEN = "token";
 
-    public static boolean saveUser(Context context, String login) {
+    public static boolean saveLogin(Context context, String login) {
         SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = prefs.edit();
         prefsEditor.putString(KEY_USER, login);
@@ -19,35 +22,35 @@ public class AuthenticationUtils {
         return true;
     }
 
-    public static boolean isUserLogged(Context context) {
-        String username = getUsername(context);
-        String password = getPassword(context);
-        return !isNull(username) && !isNull(password);
-    }
-
-    public static String getUsername(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
-        return prefs.getString(KEY_USER, null);
-    }
-
-    public static boolean savePassword(Context context, String userId) {
+    public static boolean saveToken(Context context, JWTToken token) {
         SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(KEY_PASSWORD, userId);
+        prefsEditor.putString(KEY_TOKEN, token.getToken());
         prefsEditor.apply();
         return true;
     }
 
-    public static String getPassword(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
-        return prefs.getString(KEY_PASSWORD, null);
+    public static boolean isUserLogged(Context context) {
+        String username = getLogin(context);
+        String token = getToken(context);
+
+        return !isNull(username) && !isNull(token);
     }
 
+    private static String getToken(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
+        return prefs.getString(KEY_USER, null);
+    }
+
+    public static String getLogin(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
+        return prefs.getString(KEY_USER, null);
+    }
 
     public static void resetValues(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(LOGIN_COLLECTION, Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(KEY_PASSWORD, null);
+        prefsEditor.putString(KEY_TOKEN, null);
         prefsEditor.putString(KEY_USER, null);
         prefsEditor.apply();
     }
