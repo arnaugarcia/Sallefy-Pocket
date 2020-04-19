aaf81e748ea9cd3601a03eb0aa9da002f541ddcd
emcee3 <a.martinez215@gmail.com>
2020-04-19 19:19:49 +0200

    #39 Created Search UI

--- app/src/main/java/com/sallefy/fragments/SearchFragment.java
+++ app/src/main/java/com/sallefy/fragments/SearchFragment.java
@@ -1,16 +1,21 @@
 package com.sallefy.fragments;
 
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.sallefy.R;
 
 public class SearchFragment extends Fragment {
+    private TextView mSearchTitleTextView;
 
     public SearchFragment() {
     }
@@ -26,7 +31,9 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view =  inflater.inflate(R.layout.fragment_search, container, false);
+        initViews(view);
+        return view;
     }
 
     @Override
@@ -37,5 +44,15 @@ public class SearchFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    private void initViews(View view) {
+        mSearchTitleTextView = (TextView) view.findViewById(R.id.tv_search_title);
+
+        Shader shader = new LinearGradient(0,0,0, mSearchTitleTextView.getLineHeight(),
+                ContextCompat.getColor(view.getContext(), R.color.gradientStart),
+                ContextCompat.getColor(view.getContext(), R.color.gradientEnd),
+                Shader.TileMode.MIRROR);
+        mSearchTitleTextView.getPaint().setShader(shader);
     }
 }
