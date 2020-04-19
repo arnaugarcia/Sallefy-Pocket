cd00933ffe517314a60a1d1faae18f1b2b1028d7
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-04-20 01:12:46 +0200

    #42 Made a selected track UI

--- app/src/main/java/com/sallefy/adapters/TrackListAdapter.java
+++ app/src/main/java/com/sallefy/adapters/TrackListAdapter.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TrackListAdapter extends RecyclerView.Adapter<TrackListAdapter.ViewHolder> {
@@ -43,12 +44,15 @@ public class TrackListAdapter extends RecyclerView.Adapter<TrackListAdapter.View
         holder.tvOwner.setText(track.getUser().getLogin());
         holder.tvDuration.setText(String.valueOf(track.getDuration()));
 
+        holder.tvSelectedTrackTitle.setText(track.getName());
+        holder.tvSelectedOwner.setText(track.getUser().getLogin());
+        holder.tvSelectedDuration.setText(String.valueOf(track.getDuration()));
         if (track.getThumbnail() != null) {
             Glide.with(context)
                     .asBitmap()
                     .placeholder(R.drawable.application_logo)
                     .load(track.getThumbnail())
-                    .into(holder.ivThumbnail);
+                    .into(holder.ivSelectedThumbnail);
         }
     }
 
@@ -59,20 +63,47 @@ public class TrackListAdapter extends RecyclerView.Adapter<TrackListAdapter.View
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        ImageView ivThumbnail;
+        ConstraintLayout unselectedLayout;
         TextView tvTrackTitle;
         TextView tvOwner;
         TextView tvDuration;
         AppCompatImageButton ibFavourite;
 
+        ConstraintLayout selectedLayout;
+        ImageView ivSelectedThumbnail;
+        AppCompatImageButton ibSelectedFavourite;
+        TextView tvSelectedTrackTitle;
+        TextView tvSelectedOwner;
+        TextView tvSelectedDuration;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            ivThumbnail = itemView.findViewById(R.id.iv_thumbnail);
+            unselectedLayout = itemView.findViewById(R.id.unselected_track);
+
             tvTrackTitle = itemView.findViewById(R.id.tv_track_title);
             tvOwner = itemView.findViewById(R.id.tv_owner);
             tvDuration = itemView.findViewById(R.id.tv_duration);
             ibFavourite = itemView.findViewById(R.id.ib_favourite);
+
+
+            selectedLayout = itemView.findViewById(R.id.selected_track);
+
+            ivSelectedThumbnail = itemView.findViewById(R.id.iv_selected_thumbnail);
+            tvSelectedTrackTitle = itemView.findViewById(R.id.tv_selected_track_title);
+            tvSelectedOwner = itemView.findViewById(R.id.tv_selected_owner);
+            tvSelectedDuration = itemView.findViewById(R.id.tv_selected_duration);
+            ibSelectedFavourite = itemView.findViewById(R.id.ib_selected_favourite);
+        }
+
+        public void showSelected() {
+            unselectedLayout.setVisibility(View.GONE);
+            selectedLayout.setVisibility(View.VISIBLE);
+        }
+
+        public void showUnselected() {
+            unselectedLayout.setVisibility(View.VISIBLE);
+            selectedLayout.setVisibility(View.GONE);
         }
     }
 }
