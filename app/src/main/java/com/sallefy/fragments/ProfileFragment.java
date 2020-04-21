50af672d1cfd5826473e68b7f32c878ee3e1ea5f
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-04-22 00:25:02 +0200

    #40 Made button logout click listener that opens a MaterialAlertDialog

--- app/src/main/java/com/sallefy/fragments/ProfileFragment.java
+++ app/src/main/java/com/sallefy/fragments/ProfileFragment.java
@@ -4,31 +4,27 @@ import android.content.Context;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
 import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.sallefy.R;
 import com.sallefy.managers.user.UserCallback;
 import com.sallefy.managers.user.UserManager;
 import com.sallefy.model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
 public class ProfileFragment extends Fragment implements UserCallback {
 
     private Context context;
@@ -40,18 +36,9 @@ public class ProfileFragment extends Fragment implements UserCallback {
     private TextView tvLastName;
     private TextView tvEmail;
     private TextView tvDateCreated;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageButton ibLogout;
 
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
     public ProfileFragment(Context context) {
@@ -59,31 +46,9 @@ public class ProfileFragment extends Fragment implements UserCallback {
         getUserData();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -106,6 +71,19 @@ public class ProfileFragment extends Fragment implements UserCallback {
                 ContextCompat.getColor(view.getContext(), R.color.gradientEnd),
                 Shader.TileMode.MIRROR);
         tvProfileTitle.getPaint().setShader(shader);
+
+        ibLogout.setOnClickListener(v -> new MaterialAlertDialogBuilder(context)
+                .setTitle("Logout")
+                .setMessage("Are you sure you want to logout?")
+                .setNegativeButton("No, take me back", (dialog, i) -> Toast.makeText(context, "Not logging out", Toast.LENGTH_SHORT).show())
+                .setPositiveButton("Yes, please", (dialog, i) -> Toast.makeText(context, "Logging out", Toast.LENGTH_SHORT).show())
+                .show());
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
     }
 
     private void initViews(View view) {
@@ -116,6 +94,7 @@ public class ProfileFragment extends Fragment implements UserCallback {
         tvLastName = view.findViewById(R.id.tv_last_name);
         tvEmail = view.findViewById(R.id.tv_email);
         tvDateCreated = view.findViewById(R.id.tv_date_created);
+        ibLogout = view.findViewById(R.id.ib_logout);
     }
 
     private void getUserData() {
