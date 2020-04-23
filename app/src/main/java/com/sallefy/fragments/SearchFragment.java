69d09a94722461b2f877adecac7227d52a5eeb20
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-04-23 02:11:32 +0200

    #45 Created GradientHeadline1 for Headlines with gradients and added it into Search fragment

--- app/src/main/java/com/sallefy/fragments/SearchFragment.java
+++ app/src/main/java/com/sallefy/fragments/SearchFragment.java
@@ -1,22 +1,15 @@
 package com.sallefy.fragments;
 
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.os.Bundle;
-
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
-import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.sallefy.R;
 
+import androidx.fragment.app.Fragment;
+
 public class SearchFragment extends Fragment {
-    private TextView mSearchTitleTextView;
 
     public SearchFragment() {
     }
@@ -32,9 +25,7 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_search, container, false);
-        initViews(view);
-        return view;
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     @Override
@@ -45,17 +36,5 @@ public class SearchFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-    }
-
-    private void initViews(View view) {
-        mSearchTitleTextView = view.findViewById(R.id.tv_search_title);
-
-        TextPaint paint = mSearchTitleTextView.getPaint();
-        float width = paint.measureText(mSearchTitleTextView.getText().toString());
-        Shader shader = new LinearGradient(0, 0, width, 0,
-                ContextCompat.getColor(view.getContext(), R.color.gradientStart),
-                ContextCompat.getColor(view.getContext(), R.color.gradientEnd),
-                Shader.TileMode.MIRROR);
-        mSearchTitleTextView.getPaint().setShader(shader);
     }
 }
