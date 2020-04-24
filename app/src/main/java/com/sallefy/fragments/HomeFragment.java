198cd3e2a9216615cf6083db842fd3c53c38a47a
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-04-24 19:33:07 +0200

    #55 Made HomeFragment UI

--- app/src/main/java/com/sallefy/fragments/HomeFragment.java
+++ app/src/main/java/com/sallefy/fragments/HomeFragment.java
@@ -2,7 +2,10 @@ package com.sallefy.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +14,10 @@ import android.view.ViewGroup;
 import com.sallefy.R;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView rvArtists;
+    private RecyclerView rvPlaylists;
+    private RecyclerView rvTracks;
 
     public HomeFragment() {
     }
@@ -28,6 +35,19 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initViews(view);
+    }
+
+    private void initViews(View view) {
+        rvArtists = view.findViewById(R.id.rv_artists);
+        rvPlaylists = view.findViewById(R.id.rv_playlists);
+        rvTracks = view.findViewById(R.id.rv_tracks);
     }
 
     @Override
