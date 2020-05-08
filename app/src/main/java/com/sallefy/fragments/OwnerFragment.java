d2b0e4c42dd4124022edf3f7110aa54df65a2dc4
emcee3 <a.martinez215@gmail.com>
2020-05-08 20:55:49 +0200

    #47 OwnerFragment UI implemented

---
+++ app/src/main/java/com/sallefy/fragments/OwnerFragment.java
@@ -0,0 +1,47 @@
+package com.sallefy.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.sallefy.R;
+
+public class OwnerFragment extends Fragment {
+
+    private Context mContext;
+    private FragmentManager mFragmentManager;
+
+    public OwnerFragment(Context mContext, FragmentManager mFragmentManager) {
+        this.mContext = mContext;
+        this.mFragmentManager = mFragmentManager;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_owner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
+
+    }
+
+}
